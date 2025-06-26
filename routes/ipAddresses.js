83284867c9ecef0fcc198d3e113fbff3ffc43b7f
Misa <milosursulovic2696@gmail.com>
2025-06-26 09:04:41 +0200

    Add edit and delete routes

---
+++ routes/ipAddresses.js
@@ -0,0 +1,59 @@
+import express from 'express'
+import IpEntry from '../models/IpEntry.js'
+
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+  try {
+    const entries = await IpEntry.find()
+    res.json(entries)
+  } catch (err) {
+    console.error('Error fetching IP addresses:', err)
+    res.status(500).json({ message: 'Greška na serveru' })
+  }
+})
+
+router.post('/', async (req, res) => {
+  const { ip, computerName, username, fullName, password, rdp } = req.body
+
+  if (!ip) return res.status(400).json({ message: 'IP adresa je obavezno polje' })
+
+  try {
+    const newEntry = new IpEntry({ ip, computerName, username, fullName, password, rdp })
+    await newEntry.save()
+    res.status(201).json(newEntry)
+  } catch (err) {
+    console.error('Error adding IP entry:', err)
+    res.status(500).json({ message: 'Greška na serveru' })
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  try {
+    const updated = await IpEntry.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    })
+
+    if (!updated) return res.status(404).json({ message: 'Unos nije pronađen' })
+
+    res.json(updated)
+  } catch (err) {
+    console.error('Error updating IP entry:', err)
+    res.status(500).json({ message: 'Greška na serveru' })
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const result = await IpEntry.findByIdAndDelete(req.params.id)
+    if (!result) return res.status(404).json({ message: 'Unos nije pronađen' })
+
+    res.json({ message: 'Unos obrisan' })
+  } catch (err) {
+    console.error('Error deleting IP entry:', err)
+    res.status(500).json({ message: 'Greška na serveru' })
+  }
+})
+
+
+export default router
--- routes/protected.js
+++ routes/protected.js
@@ -1,31 +1,8 @@
 import express from 'express'
-import IpEntry from '../models/IpEntry.js'
+import ipAddressesRoutes from "./ipAddresses.js";
 
 const router = express.Router()
 
-router.get('/ip-addresses', async (req, res) => {
-  try {
-    const entries = await IpEntry.find()
-    res.json(entries)
-  } catch (err) {
-    console.error('Error fetching IP addresses:', err)
-    res.status(500).json({ message: 'Greška na serveru' })
-  }
-})
-
-router.post('/ip-addresses', async (req, res) => {
-  const { ip, computerName, username, fullName, password, rdp } = req.body
-
-  if (!ip) return res.status(400).json({ message: 'IP adresa je obavezno polje' })
-
-  try {
-    const newEntry = new IpEntry({ ip, computerName, username, fullName, password, rdp })
-    await newEntry.save()
-    res.status(201).json(newEntry)
-  } catch (err) {
-    console.error('Error adding IP entry:', err)
-    res.status(500).json({ message: 'Greška na serveru' })
-  }
-})
+router.use('/ip-addresses', ipAddressesRoutes);
 
 export default router
