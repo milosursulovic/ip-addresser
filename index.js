14e138f7e179514c000e5f93f20c21f21717267f
Misa <milosursulovic2696@gmail.com>
2025-06-26 08:06:08 +0200

    Create login page

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import authRoutes from "./routes/auth.js";
+import { authenticateToken } from "./middlewares/auth.js";
 
-const app = express()
-const port = 3000
+dotenv.config();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+
+app.use("/api/auth", authRoutes);
+
+app.get("/api/protected", authenticateToken, (req, res) => {
+  res.json({ message: "This is protected data", user: req.user });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
