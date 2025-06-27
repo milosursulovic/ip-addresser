9a985e123d529f1edbca941e3b935198b7b516b7
Misa <milosursulovic2696@gmail.com>
2025-06-27 08:22:40 +0200

    Restore to http

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
-import fs from "fs";
-import https from "https";
 
 import authRoutes from "./routes/auth.js";
 import protectedRoutes from "./routes/protected.js";
@@ -13,19 +11,6 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5138;
-
-if (
-  !fs.existsSync(process.env.SSL_KEY) ||
-  !fs.existsSync(process.env.SSL_CERT)
-) {
-  console.error("❌ SSL cert or key file not found! Check .env paths.");
-  process.exit(1);
-}
-
-const sslOptions = {
-  key: fs.readFileSync(process.env.SSL_KEY),
-  cert: fs.readFileSync(process.env.SSL_CERT),
-};
 
 app.use(cors());
 app.use(express.json());
@@ -38,8 +23,6 @@ mongoose
 app.use("/api/auth", authRoutes);
 app.use("/api/protected", authenticateToken, protectedRoutes);
 
-https.createServer(sslOptions, app).listen(port, "0.0.0.0", () => {
-  console.log(
-    `🚀 Express HTTPS server running at https://10.230.62.81:${port}`
-  );
+app.listen(port, "0.0.0.0", () => {
+  console.log(`🚀 Express HTTP server running at http://10.230.62.81:${port}`);
 });
